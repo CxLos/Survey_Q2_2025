eecc257b3556b1a570c26c267953db242bad76e5
CxLos <bautistac_29@outlook.com>
2025-04-17 21:09:53 -0500

    created visuals for How clients are feeling about their health.

--- survey_q2_25.py
+++ survey_q2_25.py
@@ -99,7 +99,7 @@ df_1 = df[df['Month'] == 'January']
 df_2 = df[df['Month'] == 'February']
 df_3 = df[df['Month'] == 'March']
 
-print(df.head(10))
+# print(df.head(10))
 # print('Total Marketing Events: ', len(df))
 # print('Column Names: \n', df.columns)
 # print('DF Shape:', df.shape)
@@ -153,8 +153,8 @@ df.rename(
         'Email Address': 'Email',
         'Name:': 'Name',
         "Prior to today's visit, when was the last time you visited a doctor?": 'Last Doctor Visit',
-        'Which services were provided to you today?': 'Services Provided',
-        'How do you feel about the health issue that brought you to BMHC?': 'Health Issue Sentiment',
+        'Which services were provided to you today?': 'Service',
+        'How do you feel about the health issue that brought you to BMHC?': 'Health',
         'What is your overall stress level?': 'Stress',
         'Explain the reason for your answer:': 'Stress Explanation',
         'How would you rate your overall level of mental health?': 'Mental Health',
@@ -223,29 +223,296 @@ months_in_quarter = quarter_months[quarter]
 # ------------------------ Total Reviews ---------------------------- #
 
 total_reviews = len(df)
-# print('Total Engagements:', total_engagements)
+# print('Total Reviews:', total_engagements)
 
-# ------------------------ Engagement Hours DF ---------------------------- #
+# ------------------------ Services Provided ---------------------------- #
+
+# print("Before: \n", df[''].unique().tolist())
+# print("Value Counts: \n", df[''].unique().tolist())
+
+_unique =[
+    
+]
+
+# df[''] = (df['']
+#     .astype(str)
+#     .str.strip()
+#     .replace({
+#         "" : ""
+#     })          
+# )
+
+# print("After: \n", df[''].unique().tolist())
+
+reviews = []
+for month in months_in_quarter:
+    reviews_in_month = df[df['Month'] == month].shape[0]  # Count the number of rows for each month
+    reviews.append(reviews_in_month)
+    # print(f'Clients Served in {month}:', clients_in_month)
+
+df_reviews = pd.DataFrame(
+    {
+    'Month': months_in_quarter,
+    'Reviews': reviews
+    }
+)
+
+# print(df_)
+
+client_fig = px.bar(
+    df_reviews, 
+    x='Month', 
+    y='Reviews',
+    labels={'Reviews': 'Number of Reviews'},
+    color='Month', 
+    text='Reviews',  
+).update_layout(
+    title_x=0.5,
+    xaxis_title='Month',
+    yaxis_title='Count',
+    height=600, 
+    width = 800,
+    title=dict(
+        text= f'{current_quarter} Reviews by Month',
+        x=0.5, 
+        font=dict(
+            size=35,
+            family='Calibri',
+            color='black',
+            )
+    ),
+    font=dict(
+        family='Calibri',
+        size=17,
+        color='black'
+    ),
+    xaxis=dict(
+        title=dict(
+            text=None,
+            # text="Month",
+            font=dict(size=20), 
+        ),
+        tickmode='array',
+        tickvals=df_reviews['Month'].unique(),
+        tickangle=0  
+    ),
+    legend=dict(
+        # title='Administrative Activity',
+        title=None,
+        orientation="v",  # Vertical legend
+        x=1.05,  # Position legend to the right
+        xanchor="left",  # Anchor legend to the left
+        y=1,  # Position legend at the top
+        yanchor="top" 
+    ),
+).update_traces(
+    texttemplate='%{text}',
+    textfont=dict(size=20),  
+    textposition='auto', 
+    textangle=0, 
+    hovertemplate=( 
+        '<b>Name</b>: %{label}<br><b>Count</b>: %{y}<extra></extra>'  
+    ),
+)
+
+client_pie = px.pie(
+    df_reviews,
+    names='Month',
+    values='Reviews',
+    color='Month',
+    height=550
+).update_layout(
+    title=dict(
+        x=0.5,
+        text=f'{current_quarter} Ratio of Clients Served', 
+        font=dict(
+            size=35,  
+            family='Calibri',  
+            color='black'  
+        ),
+    ),  
+    margin=dict(
+        l=0, 
+        r=0,  
+        t=100,  
+        b=0   
+    )  
+).update_traces(
+    rotation=180,  #
+    textfont=dict(size=19),  
+    textinfo='value+percent',
+    # texttemplate='<br>%{percent:.0%}', 
+    hovertemplate='<b>%{label}</b>: %{value}<extra></extra>'
+)
+
+# ------------------------ Health Issue ---------------------------- #
+
+# The code is printing the unique values in the 'Health' column of a DataFrame called `df` as a list.
+# print("Unique Health Before: \n", df['Health'].unique().tolist())
+print("Health Value Counts: \n", df['Health'].value_counts())
+
+_unique =[
+    
+]
+
+df['Health'] = (df['Health']
+    .astype(str)
+    .str.strip()
+    .replace({
+        "" : ""
+    })          
+)
+
+# print("After: \n", df[''].unique().tolist())
+
+df_health = df['Health'].value_counts().reset_index(name='Count')
+
+# health_reviews = []
+# for month in months_in_quarter:
+#     health_reviews_in_month = df[df['Month'] == month].shape[0]  # Count the number of rows for each month
+#     reviews.append(reviews_in_month)
+#     # print(f'Clients Served in {month}:', clients_in_month)
+
+# df_reviews = pd.DataFrame(
+#     {
+#     'Month': months_in_quarter,
+#     'Reviews': reviews
+#     }
+# )
+
+df_health_counts = (
+    df.groupby(['Month', 'Health'], sort=False)
+    .size()
+    .reset_index(name='Count')
+)
+
+df_health_counts['Month'] = pd.Categorical(
+    df_health_counts['Month'],
+    categories = months_in_quarter,
+    ordered = True
+)
+
+df_health_counts = df_health_counts.sort_values(['Month', 'Health'])
+
+# print(df_)
+
+health_fig = px.bar(
+    df_health_counts, 
+    x='Month', 
+    y='Count',
+    color='Health', 
+    text='Count',  
+    barmode='group',
+    labels={
+        'Count': 'Count',
+        'Month': 'Month',
+        'Health': 'Health'
+    },
+).update_layout(
+    title_x=0.5,
+    xaxis_title='Month',
+    yaxis_title='Count',
+    height=600, 
+    width = 800,
+    title=dict(
+        text= f'{current_quarter} How Clients are feeling about their health',
+        x=0.5, 
+        font=dict(
+            size=25,
+            family='Calibri',
+            color='black',
+            )
+    ),
+    font=dict(
+        family='Calibri',
+        size=17,
+        color='black'
+    ),
+    xaxis=dict(
+        title=dict(
+            text=None,
+            # text="Month",
+            font=dict(size=20), 
+        ),
+        tickmode='array',
+        tickvals=df_reviews['Month'].unique(),
+        tickangle=0  
+    ),
+    legend=dict(
+        title='Rating',
+        # title=None,
+        orientation="v",  # Vertical legend
+        x=1.05,  # Position legend to the right
+        xanchor="left",  # Anchor legend to the left
+        y=1,  # Position legend at the top
+        yanchor="top" 
+    ),
+).update_traces(
+    texttemplate='%{text}',
+    textfont=dict(size=20),  
+    textposition='auto', 
+    textangle=0, 
+    hovertemplate=( 
+        '<b>Name</b>: %{label}<br><b>Count</b>: %{y}<extra></extra>'  
+    ),
+)
+
+health_pie = px.pie(
+    df_health,
+    names='Health',
+    values='Count',
+    color='Health',
+    height=550
+).update_layout(
+    title=dict(
+        x=0.5,
+        text=f'{current_quarter} Ratio of How Clients are feeling about their health', 
+        font=dict(
+            size=25,  
+            family='Calibri',  
+            color='black'  
+        ),
+    ),  
+    legend=dict(
+        title='Rating',
+        # title=None,
+        orientation="v",  # Vertical legend
+        x=1.05,  # Position legend to the right
+        xanchor="left",  # Anchor legend to the left
+        y=1,  # Position legend at the top
+        yanchor="top" 
+    ),
+    margin=dict(
+        l=0, 
+        r=0,  
+        t=100,  
+        b=0   
+    )  
+).update_traces(
+    rotation=180,  #
+    textfont=dict(size=19),  
+    textinfo='value+percent',
+    # texttemplate='<br>%{percent:.0%}', 
+    hovertemplate='<b>%{label}</b>: %{value}<extra></extra>'
+)
+
+# --------------------------------- Stress Level -------------------------------- #
 
 
 
-# ------------------------ Total Travel Time DF ---------------------------- #
+# --------------------- Mental Health ------------------------ # 
 
 
 
-# --------------------------------- Activity Status DF -------------------------------- #
+# --------------------- Physical Health ------------------------ #
 
 
 
-# --------------------- Administrative Activity DF ------------------------ # 
-
-
-
-# --------------------- Care Network Activity DF ------------------------ #
-
-
-
-# ------------------------ Person Submitting Form DF ---------------------------- #
+# ------------------------ Impression of BMHC ? ---------------------------- #
+# ------------------------ Provider Expectation ---------------------------- #
+# ------------------------ Care Needs ---------------------------- #
+# ------------------------ Outreach Support ---------------------------- #
+# ------------------------ Healthy Cuts Membership ---------------------------- #
 
 
 
@@ -280,31 +547,6 @@ engagement_table.update_layout(
     plot_bgcolor='rgba(0,0,0,0)'  # Transparent plot area
 )
 
-# Entity Name Table
-# entity_name_table = go.Figure(data=[go.Table(
-#     header=dict(
-#         values=list(entity_name_group.columns),
-#         fill_color='paleturquoise',
-#         align='center',
-#         height=30,
-#         font=dict(size=12)
-#     ),
-#     cells=dict(
-#         values=[entity_name_group[col] for col in entity_name_group.columns],
-#         fill_color='lavender',
-#         align='left',
-#         height=25,
-#         font=dict(size=12)
-#     )
-# )])
-
-# entity_name_table.update_layout(
-#     margin=dict(l=50, r=50, t=30, b=40),
-#     height=400,
-#     paper_bgcolor='rgba(0,0,0,0)',
-#     plot_bgcolor='rgba(0,0,0,0)'
-# )
-
 # ============================== Dash Application ========================== #
 
 app = dash.Dash(__name__)
@@ -316,7 +558,7 @@ app.layout = html.Div(
         className='divv', 
         children=[ 
           html.H1(
-              f'BMHC Review Submissions Report {current_quarter} {report_year}', 
+              f'BMHC Client Review Report {current_quarter} {report_year}', 
               className='title'),
           html.H2( 
               '01/01/2025 - 3/31/2024', 
@@ -326,40 +568,40 @@ app.layout = html.Div(
               children=[
                   html.A(
                     'Repo',
-                    href= f'https://github.com/CxLos/Eng_{current_quarter}_2025',
+                    href= f'https://github.com/CxLos/Survey_{current_quarter}_{report_year}',
                     className='btn'),
     ]),
   ]),    
 
 # Data Table
-html.Div(
-    className='row00',
-    children=[
-        html.Div(
-            className='graph00',
-            children=[
-                html.Div(
-                    className='table',
-                    children=[
-                        html.H1(
-                            className='table-title',
-                            children='Engagements Table'
-                        )
-                    ]
-                ),
-                html.Div(
-                    className='table2', 
-                    children=[
-                        dcc.Graph(
-                            className='data',
-                            # figure=engagement_table
-                        )
-                    ]
-                )
-            ]
-        ),
-    ]
-),
+# html.Div(
+#     className='row00',
+#     children=[
+#         html.Div(
+#             className='graph00',
+#             children=[
+#                 html.Div(
+#                     className='table',
+#                     children=[
+#                         html.H1(
+#                             className='table-title',
+#                             children='Client Review Table'
+#                         )
+#                     ]
+#                 ),
+#                 html.Div(
+#                     className='table2', 
+#                     children=[
+#                         dcc.Graph(
+#                             className='data',
+#                             figure=survey_table
+#                         )
+#                     ]
+#                 )
+#             ]
+#         ),
+#     ]
+# ),
 
 # ROW 1
 html.Div(
@@ -370,7 +612,7 @@ html.Div(
             children=[
             html.Div(
                 className='high1',
-                children=[f'{current_quarter} Engagements']
+                children=[f'{current_quarter} Reviews']
             ),
             html.Div(
                 className='circle1',
@@ -380,7 +622,7 @@ html.Div(
                         children=[
                             html.H1(
                             className='high3',
-                            # children=[total_engagements]
+                            children=[total_reviews]
                     ),
                         ]
                     )
@@ -394,7 +636,7 @@ html.Div(
             children=[
             html.Div(
                 className='high2',
-                children=[f'{current_quarter} Engagement Hours']
+                children=[f'{current_quarter} Placeholder']
             ),
             html.Div(
                 className='circle2',
@@ -404,7 +646,7 @@ html.Div(
                         children=[
                             html.H1(
                             className='high4',
-                            # children=[eng_hours]
+                            # children=[]
                     ),
                         ]
                     )
@@ -425,7 +667,7 @@ html.Div(
             children=[
             html.Div(
                 className='high1',
-                children=[f'{current_quarter} Travel Hours']
+                children=[f'{current_quarter} Placeholder']
             ),
             html.Div(
                 className='circle1',
@@ -435,7 +677,7 @@ html.Div(
                         children=[
                             html.H1(
                             className='high3',
-                            # children=[total_travel_time]
+                            # children=[]
                     ),
                         ]
                     )
@@ -456,27 +698,27 @@ html.Div(
 ),
 
 # ROW 1
-# html.Div(
-#     className='row1',
-#     children=[
-#         html.Div(
-#             className='graph1',
-#             children=[
-#                 dcc.Graph(
-#                     # figure=status_fig
-#                 )
-#             ]
-#         ),
-#         html.Div(
-#             className='graph2',
-#             children=[
-#                 dcc.Graph(
-#                     # figure=status_fig
-#                 )
-#             ]
-#         ),
-#     ]
-# ),
+html.Div(
+    className='row1',
+    children=[
+        html.Div(
+            className='graph1',
+            children=[
+                dcc.Graph(
+                    figure=health_fig
+                )
+            ]
+        ),
+        html.Div(
+            className='graph2',
+            children=[
+                dcc.Graph(
+                    figure=health_pie
+                )
+            ]
+        ),
+    ]
+),
 
 # ROW 1
 html.Div(
@@ -646,13 +888,13 @@ html.Div(
 
 print(f"Serving Flask app '{current_file}'! 🚀")
 
-# if __name__ == '__main__':
-#     app.run_server(debug=
-#                    True)
+if __name__ == '__main__':
+    app.run_server(debug=
+                   True)
                 #    False)
 # =================================== Updated Database ================================= #
 
-# updated_path = f'data/Engagement_{current_quarter}_{report_year}.xlsx'
+# updated_path = f'data/Survey_{current_quarter}_{report_year}.xlsx'
 # data_path = os.path.join(script_dir, updated_path)
 # df.to_excel(data_path, index=False)
 # print(f"DataFrame saved to {data_path}")
@@ -733,3 +975,121 @@ print(f"Serving Flask app '{current_file}'! 🚀")
 # rm -rf ~$bmhc_data_2024_cleaned.xlsx
 # rm -rf ~$bmhc_data_2024.xlsx
 # rm -rf ~$bmhc_q4_2024_cleaned2.xlsx
+
+# print("Before: \n", df[''].unique().tolist())
+# print("Value Counts: \n", df[''].unique().tolist())
+
+_unique =[
+    
+]
+
+# df[''] = (df['']
+#     .astype(str)
+#     .str.strip()
+#     .replace({
+#         "" : ""
+#     })          
+# )
+
+# print("After: \n", df[''].unique().tolist())
+
+b = []
+for month in months_in_quarter:
+    b_in_month = df[df['Month'] == month].shape[0]  # Count the number of rows for each month
+    b.append(b_in_month)
+    # print(f'Clients Served in {month}:', clients_in_month)
+
+df_ = pd.DataFrame(
+    {
+    'Month': months_in_quarter,
+    'Reviews': b
+    }
+)
+
+# print(df_)
+
+client_fig = px.bar(
+    df_, 
+    x='Month', 
+    y='',
+    labels={'': 'Number of Reviews'},
+    color='Month', 
+    text='Reviews',  
+).update_layout(
+    title_x=0.5,
+    xaxis_title='Month',
+    yaxis_title='Count',
+    height=600, 
+    width = 800,
+    title=dict(
+        text= f'{current_quarter} Reviews by Month',
+        x=0.5, 
+        font=dict(
+            size=35,
+            family='Calibri',
+            color='black',
+            )
+    ),
+    font=dict(
+        family='Calibri',
+        size=17,
+        color='black'
+    ),
+    xaxis=dict(
+        title=dict(
+            text=None,
+            # text="Month",
+            font=dict(size=20), 
+        ),
+        tickmode='array',
+        tickvals=df_reviews['Month'].unique(),
+        tickangle=0  
+    ),
+    legend=dict(
+        # title='Administrative Activity',
+        title=None,
+        orientation="v",  # Vertical legend
+        x=1.05,  # Position legend to the right
+        xanchor="left",  # Anchor legend to the left
+        y=1,  # Position legend at the top
+        yanchor="top" 
+    ),
+).update_traces(
+    texttemplate='%{text}',
+    textfont=dict(size=20),  
+    textposition='auto', 
+    textangle=0, 
+    hovertemplate=( 
+        '<b>Name</b>: %{label}<br><b>Count</b>: %{y}<extra></extra>'  
+    ),
+)
+
+client_pie = px.pie(
+    df_reviews,
+    names='Month',
+    values='Reviews',
+    color='Month',
+    height=550
+).update_layout(
+    title=dict(
+        x=0.5,
+        text=f'{current_quarter} Ratio of Clients Served', 
+        font=dict(
+            size=35,  
+            family='Calibri',  
+            color='black'  
+        ),
+    ),  
+    margin=dict(
+        l=0, 
+        r=0,  
+        t=100,  
+        b=0   
+    )  
+).update_traces(
+    rotation=180,  #
+    textfont=dict(size=19),  
+    textinfo='value+percent',
+    # texttemplate='<br>%{percent:.0%}', 
+    hovertemplate='<b>%{label}</b>: %{value}<extra></extra>'
+)
